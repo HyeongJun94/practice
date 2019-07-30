a79cbf5e1701a235d297f2b6dcaa9c2f52caa513
hyeongjun94 <whow1101@naver.com>
2019-07-30 15:16:03 +0900

    practice context

---
+++ context-tutorial/src/contexts/sample.js
@@ -0,0 +1,31 @@
+import React, { Component, createContext } from 'react';
+
+const Context = createContext();
+
+const { Provider, Consumer: SampleConsumer } = Context;
+
+class SampleProvider extends Component{
+  state = {
+    value: '기본입니디다'
+  }
+  actions = {
+    setValue: (value) => {
+      this.setState({value});
+    }
+  }
+
+  render(){
+    const {state, actions} = this;
+
+    const value = {state, actions};
+
+    return(
+      <Provider value={value}>
+        {this.props.children}
+      </Provider>
+    )
+  }
+}
+
+export {SampleProvider, SampleConsumer};
+
